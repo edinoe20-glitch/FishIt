36a297f49e31f450d8e9650987f1d8fda6fedc86
edinoe20-glitch <edinoe20@gmail.com>
2026-01-02 18:02:11 +0700

    Update Discord_Notifier.lua

--- Modules/Discord_Notifier.lua
+++ Modules/Discord_Notifier.lua
@@ -1,1 +1,32 @@
+-- [[ MODULE: DISCORD NOTIFIER ]] --
+local HttpService = game:GetService("HttpService")
+
+local Module = {}
+
+function Module.Send(fishName, weight)
+    local WebhookURL = "https://discord.com/api/webhooks/1456380736641831086/lRLi_uAxOXZ5b2GjBrP370x0EIKA0VPmJNeWkteXGs6xlWPAOjsjFx1PoSMYnkKg4ikJ"
+    local ProxyURL = WebhookURL:gsub("discord.com", "webhook.lewisakura.moe") 
+    
+    local payload = HttpService:JSONEncode({
+        ["embeds"] = {{
+            ["title"] = "🎣 NEW CATCH!",
+            ["description"] = "Ikan: " .. tostring(fishName) .. "\nBerat: " .. tostring(weight) .. " Kg",
+            ["color"] = 3066993
+        }}
+    })
+    
+    local req = request or http_request or (syn and syn.request)
+    if req then
+        pcall(function()
+            req({
+                Url = ProxyURL,
+                Method = "POST",
+                Headers = {["Content-Type"] = "application/json"},
+                Body = payload
+            })
+        end)
+    end
+end
+
+return Module
 
